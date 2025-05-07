997ed6e833a0e56b36b5d5b08d91e8845a1a4517
Torsten Schlieder <torsten.schlieder@unite.eu>
2025-05-07 15:32:30 +0200

    Central schema merging

--- src/mergedSchema.ts
+++ src/mergedSchema.ts
@@ -3,11 +3,13 @@ import { localResolvers } from './localResolvers'
 import { getRemoteSchema } from './remoteSchema'
 import { mergeSchemas } from '@graphql-tools/schema'
 
-// Requires the remove GraphQL to be running on startup
-const remoteSchema = await getRemoteSchema('http://localhost:8080/graphql')
+const remoteSchemas = await Promise.all([
+    getRemoteSchema('http://localhost:8080/graphql')
+    // Add additional remote schema requests here
+])
 
 export const mergedSchema = mergeSchemas({
-    schemas: [remoteSchema],
+    schemas: remoteSchemas,
     typeDefs: localTypeDefs,
     resolvers: [localResolvers]
 })
